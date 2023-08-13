4d6c693e586156f0009d9d514883cc2e1442f6ca
Mark Romphf <markromphf@gmail.com>
2023-08-13 15:57:02 -0600

    Rename variable to squash warning.

--- Board.gd
+++ Board.gd
@@ -6,8 +6,8 @@ func reset():
 	for node in $OutputHousing.get_children():
 		node.reset()
 
-func update(round, input, red, white):
-	var target = $OutputHousing.get_children()[max(0, round - 1)]
+func update(rnd, input, red, white):
+	var target = $OutputHousing.get_children()[max(0, rnd - 1)]
 	target.render_submission(input)
 	target.render_hints(red, white)
 
