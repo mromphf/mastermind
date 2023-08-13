bea3c1a4428ba30efe5eef5e55b0c85fd74c641c
Mark Romphf <markromphf@gmail.com>
2023-08-13 14:51:23 -0600

    Timed hints.

---
+++ Hints.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+var _hints = []
+var _idx = 0
+
+func _tick():
+	if not _hints.is_empty():
+		$Control.get_children()[_idx].set_texture(_hints.pop_back())
+		_idx += 1
+	else:
+		$Timer.stop()
+
+
+func reset():
+	for h in $Control.get_children():
+		h.set_texture(Code.hint_map[Code.Hints.EMPTY])
+
+
+func render(hints: Array):
+	_hints = hints
+	$Timer.start()
--- output.gd
+++ output.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 var inputs = {}
 
+
+
 func render_submission(submission):
 	for idx in inputs.keys():
 		inputs[idx].set_texture(Code.image_map[submission[idx]])
@@ -9,11 +11,17 @@ func render_submission(submission):
 
 func render_hints(correct: int, white: int):
 	var hints = $Hints.get_children()
+
 	for hint in hints.slice(0, correct):
 		hint.set_texture(Code.hint_map[2])
 
 	for hint in hints.slice(correct, correct + white):
 		hint.set_texture(Code.hint_map[1])
+
+	var reds = range(correct).map(func(_x): return Code.hint_map[Code.Hints.RED])
+	var whites = range(white).map(func(_x): return Code.hint_map[Code.Hints.WHITE])
+
+	$Hints_new.render(whites + reds)
 
 
 func reset():
@@ -22,6 +30,8 @@ func reset():
 
 	for h in $Hints.get_children():
 		h.set_texture(Code.EMPTY_CIRCLE)
+		
+	$Hints_new.reset()
 
 func _ready():
 	inputs = {
