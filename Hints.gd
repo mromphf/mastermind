6a8afe31438d675eccd6b379663bb1ee6b6f3ab1
Mark Romphf <markromphf@gmail.com>
2023-08-13 15:38:40 -0600

    Use stack instead of index.

--- Hints.gd
+++ Hints.gd
@@ -1,23 +1,22 @@
 extends Node2D
 
 var _hints = []
-var _idx = 0
+var _nodes = []
 
 func _tick():
 	if not _hints.is_empty():
 		$Beep.play()
-		$Control.get_children()[_idx].set_texture(_hints.pop_back())
-		_idx += 1
+		_nodes.pop_front().set_texture(_hints.pop_back())
 	else:
 		$Timer.stop()
 
 
 func reset():
-	_idx = 0
 	for h in $Control.get_children():
 		h.set_texture(Code.hint_map[Code.Hints.EMPTY])
 
 
 func render(hints: Array):
 	_hints = hints
+	_nodes = $Control.get_children()
 	$Timer.start()
