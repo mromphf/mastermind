94cb029bb64c9170b68c75602947de9878243d50
Mark Romphf <markromphf@gmail.com>
2023-08-13 19:19:45 -0600

    Messing with layout.

--- GameOver.gd
+++ GameOver.gd
@@ -5,12 +5,12 @@ signal play_again
 
 func render(code: Dictionary, victory: bool = false):
 	self.visible = true
-	$Victory.visible = victory
-	$Fail.visible = not victory
-	$Code/Block1.set_texture(Code.image_map[code[1]])
-	$Code/Block2.set_texture(Code.image_map[code[2]])
-	$Code/Block3.set_texture(Code.image_map[code[3]])
-	$Code/Block4.set_texture(Code.image_map[code[4]])
+	$Center/Victory.visible = victory
+	$Center/Fail.visible = not victory
+	$Center/Code/Block1.set_texture(Code.image_map[code[1]])
+	$Center/Code/Block2.set_texture(Code.image_map[code[2]])
+	$Center/Code/Block3.set_texture(Code.image_map[code[3]])
+	$Center/Code/Block4.set_texture(Code.image_map[code[4]])
 
 
 func _on_play_again():
